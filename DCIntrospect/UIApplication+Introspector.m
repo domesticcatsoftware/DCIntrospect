a8fe1da30e813ef586167ddf4fb74f2e7569493c
C. Bess <cbess@quantumquinn.com>
2012-05-03 12:20:53 -0500

    - refactored sendEvent override/replace to modify UIApplication; renamed CBSharedConstants.h to CBSharedHeader.h

---
+++ DCIntrospect/UIApplication+Introspector.m
@@ -0,0 +1,13 @@
+//
+//  UIApplication+Introspector.m
+//  DCIntrospectDemo
+//
+//  Created by Christopher Bess on 5/3/12.
+//  Copyright (c) 2012 Christopher Bess. All rights reserved.
+//
+
+#import "UIApplication+Introspector.h"
+
+@implementation UIApplication (Introspector)
+
+@end
