2a4954c91a119a21f796d635819ed86bf0e5f9f3
Anders D. Johnson <anders.johnson@jostens.com>
2015-06-17 10:37:37 -0500

    Only enable debugger on X86 and X64, when not ARM. Fixes #72.

--- DCIntrospect/DCIntrospect.m
+++ DCIntrospect/DCIntrospect.m
@@ -52,8 +52,11 @@ static bool AmIBeingDebugged(void)
 #if TARGET_CPU_ARM
 #define DEBUGSTOP(signal) __asm__ __volatile__ ("mov r0, %0\nmov r1, %1\nmov r12, %2\nswi 128\n" : : "r"(getpid ()), "r"(signal), "r"(37) : "r12", "r0", "r1", "cc");
 #define DEBUGGER do { int trapSignal = AmIBeingDebugged () ? SIGINT : SIGSTOP; DEBUGSTOP(trapSignal); if (trapSignal == SIGSTOP) { DEBUGSTOP (SIGINT); } } while (false);
-#else
+#elif TARGET_CPU_X86 || TARGET_CPU_X86_64
 #define DEBUGGER do { int trapSignal = AmIBeingDebugged () ? SIGINT : SIGSTOP; __asm__ __volatile__ ("pushl %0\npushl %1\npush $0\nmovl %2, %%eax\nint $0x80\nadd $12, %%esp" : : "g" (trapSignal), "g" (getpid ()), "n" (37) : "eax", "cc"); } while (false);
+#else
+#define DEBUGGER
+#warning "Debugger disabled."
 #endif
 
 @interface DCIntrospect ()
